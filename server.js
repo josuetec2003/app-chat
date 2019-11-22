39cd612ad542095414c8722e2a3623168c39ef33
Josue Alvarez <josuetec2003@gmail.com>
2019-11-22 15:10:21 -0600

    Estructura html inicial del proyecto

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const PORT = 11000
+
+app.set('view engine', 'pug')
+app.use(express.static(__dirname + '/static'))
+
+app.get('/', (req, res) => {
+	res.render('index')
+})
+
+server.listen(PORT, () => {
+	console.log(`Escuchando en el puerto ${PORT}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
